d3d706906f1e9a6c5bd5c882ae23655b220cbde4
Ashit Mittal <45489842+ashit21j@users.noreply.github.com>
2020-08-09 23:23:03 +0200

    Create variables.tf
    Initial commit

---
+++ Internet/variables.tf
@@ -0,0 +1,31 @@
+variable "Egress-vpc-cidr" {
+  default = "10.4.0.0/16"
+}
+
+variable "Egress-Private-subnet1a-cidr" {
+  default = "10.4.1.0/24"
+}
+
+variable "Egress-Private-subnet1b-cidr" {
+  default = "10.4.2.0/24"
+}
+
+variable "Egress-Private-subnet1c-cidr" {
+  default = "10.4.3.0/24"
+}
+
+variable "Egress-Public-subnet1a-cidr" {
+  default = "10.4.4.0/24"
+}
+
+variable "Egress-Public-subnet1b-cidr" {
+  default = "10.4.5.0/24"
+}
+
+variable "Egress-Public-subnet1c-cidr" {
+  default = "10.4.6.0/24"
+}
+
+variable "EgressSubnet-cidr" {
+  default = "10.4.7.0/24"
+}
