68c956ceaa0166face62b79eca079e092b2516c8
Ashit Mittal <45489842+ashit21j@users.noreply.github.com>
2020-08-09 23:09:55 +0200

    Create variables.tf
    Initial commit

---
+++ Production/variables.tf
@@ -0,0 +1,15 @@
+variable "Prod-vpc-cidr" {
+    default = "10.3.0.0/16"
+}
+
+variable "Prod-subnet1a-cidr" {
+    default = "10.3.1.0/24"
+}
+
+variable "Prod-subnet1b-cidr" {
+    default = "10.3.2.0/24"
+}
+
+variable "Prod-subnet1c-cidr" {
+    default = "10.3.3.0/24"
+}
