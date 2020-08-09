feb72197433120d1a0a0e5b515dafa03eec34132
Ashit Mittal <45489842+ashit21j@users.noreply.github.com>
2020-08-09 23:09:03 +0200

    Create variables.tf
    Initial commit

---
+++ Gateway/variables.tf
@@ -0,0 +1,15 @@
+variable "Gateway-vpc-cidr" {
+    default = "10.1.0.0/16"
+}
+
+variable "Gateway-subnet1-cidr" {
+    default = "10.1.1.0/24"
+}
+
+variable "Gateway-subnet2-cidr" {
+    default = "10.1.2.0/24"
+}
+
+variable "Gateway-subnet3-cidr" {
+    default = "10.1.3.0/24"
+}
